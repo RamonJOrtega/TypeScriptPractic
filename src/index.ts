d1296d755acefa7534fdb1fba175214b126dc45a
ramon6767 <ramon.justis.ortega@gmail.com>
2023-03-29 12:06:41 -0700

    add longestPalendrom function to index.ts, but it only creates the hashmap so far, needs to axtually analyze it

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,20 @@ console.log('hello')
 
 const chicken = new Chicken()
 chicken.cluck()
+
+const longestPalendrom = (string: string) => {
+    const letterObj: {[key: string]: number} = {};
+    for (let i = 0; i < string.length; i++) {
+        const letter = string[i];
+        if (letterObj[letter]) {
+            letterObj[letter] = letterObj[letter] + 1;
+        } else {
+            letterObj[letter] = 1;
+        }
+        
+    }
+    console.log(letterObj)
+
+}
+
+longestPalendrom("wowee")
